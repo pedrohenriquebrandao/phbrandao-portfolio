0149dd3d520dedd95195d880d054481cbe9687bb
Pedro Brandão <phbrandao@outlook.com>
2022-07-29 11:47:28 -0300

    ..

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
 		<div className="w-full h-16 bg-gray-900 flex justify-center items-center">
 			<h1 className="text-xl text-indigo-300 mr-2">Made with</h1>{" "}
 			<a target="_blank" href="https://nextjs.org" rel="noreferrer">
-				<SiNextdotjs size={25} className="text-gray-300" />
+				<SiNextdotjs size={25} className="text-gray-300 ml-2" />
 			</a>
 			{/* <h1 className="text-xl text-blue-900 ml-2">
 				<a
