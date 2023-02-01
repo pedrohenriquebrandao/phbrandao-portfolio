63c69b62aeaeea94f3a028da1009c02941b37c3a
Pedro Brandão <phbrandao@outlook.com>
2023-02-01 14:08:14 -0300

    Updated Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,15 +19,14 @@ function Navbar() {
 							
 							<div className="flex justify-center items-center flex-shrink-1">
 								<div className="cursor-pointer hidden md:block">
-								<div className="inline-flex items-center px-3 py-2 mr-1 gap-1 rounded px-4 py-2 leading-5 text-primary-100 ">
-								<MdOutlineMail className="text-gray-400" size={25}/> <p className="text-gray-400">phbrandao@outlook.com</p>
-								<a target="_blank" href="cv_pedro.pdf" className="inline-flex text-white bg-indigo-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 mb-0 ml-4 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-									<BsDownload className="mr-3" size={20}/>
-									<p className="text-sm uppercase text-gray-200">Download CV</p>
-								</a>
-							</div>
+									<div className="inline-flex items-center px-3 py-2 mr-1 gap-1 rounded px-4 py-2 leading-5 text-primary-100 ">
+										<MdOutlineMail className="text-gray-400" size={25}/> <p className="text-gray-400">phbrandao@outlook.com</p>
+										{/* <a target="_blank" href="cv_pedro.pdf" className="inline-flex text-white bg-indigo-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 mb-0 ml-4 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+											<BsDownload className="mr-3" size={20}/>
+											<p className="text-sm uppercase text-gray-200">Download CV</p>
+										</a> */}
+									</div>
 								</div>
-								
 							</div>
 						</div>
 						<div className="mr-14 flex md:hidden ">
